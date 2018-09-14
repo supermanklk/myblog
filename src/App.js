3a66d885c0233315cc7213153cfcd34590d085c2
Metalfish <metalfish@MetalfishdeMac-Pro-4.local>
2018-09-14 17:37:49 +0800

    第一次提交，也是测试一下

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Button } from 'antd';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Button type="danger">Danger</Button>
+      </div>
+    );
+  }
+} 
+
+export default App;
