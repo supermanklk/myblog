8b677e317967990e90fe84d84ccc1643fb847196
supermanzhangbin <739367755@qq.com>
2018-10-11 17:16:34 +0800

    修改了音乐列表
    1

--- src/pages/home/containers/music/index.js
+++ src/pages/home/containers/music/index.js
@@ -4,23 +4,46 @@ import React from 'react';
 // import {bindActionCreators} from 'redux';
 import {Table, Column, Cell} from 'fixed-data-table-2';
 import 'fixed-data-table-2/dist/fixed-data-table.css';
+// fixed-data-table 的响应式 
+import Dimensions from 'react-dimensions';
 class Music extends React.Component {
   
 	render() {
+
 		const data = [ 
 			{a : '让一切随风',b : '钟镇涛', c : '听涛'},
 			{a : '一言难尽',b : '张宇', c : '一言难尽'},
 			{a : '小半',b : '陈粒', c : '小梦大半'},
+			{a : '小半',b : '陈粒', c : '小梦大半'},
+			{a : '小半',b : '陈粒', c : '小梦大半'},
+			{a : '小半',b : '陈粒', c : '小梦大半'},
+			{a : '小半',b : '陈粒', c : '小梦大半'},
+			{a : '小半',b : '陈粒', c : '小梦大半'},
+			{a : '小半',b : '陈粒', c : '小梦大半'},
+			{a : '小半',b : '陈粒', c : '小梦大半'},
+			{a : '小半',b : '陈粒', c : '小梦大半'},
+			{a : '小半',b : '陈粒', c : '小梦大半'},
+			{a : '小半',b : '陈粒', c : '小梦大半'},
+			{a : '小半',b : '陈粒', c : '小梦大半'},
+			{a : '小半',b : '陈粒', c : '小梦大半'},
+			{a : '小半',b : '陈粒', c : '小梦大半'},
+			{a : '小半',b : '陈粒', c : '小梦大半'},
+			{a : '小半',b : '陈粒', c : '小梦大半'},
+			{a : '小半',b : '陈粒', c : '小梦大半'},
+			{a : '小半',b : '陈粒', c : '小梦大半'},
+			{a : '小半',b : '陈粒', c : '小梦大半'},
+			{a : '小半',b : '陈粒', c : '小梦大半'},
+			{a : '小半',b : '陈粒', c : '小梦大半'},
 		  ];
 		return (
 			<div>
-				  <div> 
+				  <div>  
 					<Table
 					rowHeight={50}  //行高
 					rowsCount={data.length} //行数
-					width={500} //table宽度
-					height={200} //table的高度s
-					headerHeight={50} //header的高度
+					width={this.props.containerWidth} //table宽度
+					height={this.props.containerHeight} //table的高度s
+					headerHeight={50} //header的高度 
 				>
 				<Column 
 					header = {<Cell>歌曲</Cell>}
@@ -69,4 +92,12 @@ class Music extends React.Component {
 
 // export default connect(mapStateToProps, mapDispatchToProps)(Learn);
 
-export default Music;
+export default (Dimensions({
+	getHeight : function (element) {
+		return window.innerHeight - 100;
+	},
+	getWidth : function (element) {
+		return window.innerWidth;
+	}
+})(Music));
+
